0c7f472fba015fe3fb5282445cc7fce78a4624bb
SamStan4 <sam.charles.stanley@gmail.com>
2025-06-16 22:41:14 -0700

    added project generation command

---
+++ ProjectGen.sh
@@ -0,0 +1,11 @@
+#!/usr/env/bin bash
+
+mvn archetype:generate \
+        -DarchetypeGroupId=org.openjfx \
+        -DarchetypeArtifactId=javafx-archetype-simple \
+        -DarchetypeVersion=0.0.6 \
+        -DgroupId=io.github.samstan4 \
+        -DartifactId=ExHell \
+        -Dversion=1.0.0 \
+        -Djavafx-version=21.0.7
+
